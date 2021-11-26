8c9daa54324773f1fa4248ad99251023c2b4ad9a
yannick Djemani <yanger842@gmail.com>
2021-11-26 16:28:48 -0500

    add logic in my controller , add page to register and list all brand and add error page

---
+++ src/main/java/com/codeimmig/yannick/controller/BrandController.java
@@ -0,0 +1,48 @@
+package com.codeimmig.yannick.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.codeimmig.yannick.entity.Brand;
+import com.codeimmig.yannick.service.IBrandService;
+
+@Controller
+@RequestMapping("/brand")
+public class BrandController {
+	@Autowired
+	private IBrandService service;
+	//1. show Register page
+		@GetMapping("register")
+		public String showReg() {
+			return "BrandRegister";
+		}
+		
+		//2. save brand 
+		@PostMapping("/save")
+		public String saveBrand(
+				@ModelAttribute Brand brand,
+				Model model
+				) 
+		{
+			Long id = service.saveBrand(brand);
+			String message = "Brand '"+id+"' is created";
+			model.addAttribute("message", message);
+			return "BrandRegister";
+		}
+		
+		//3. fetch all brands
+		@GetMapping("/all")
+		public String getAllBrands(Model model) 
+		{
+			List<Brand> list = service.getAllBrands();
+			model.addAttribute("list", list);
+			return "BrandData";
+		}
+}
